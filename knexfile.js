4c73027e231d83a860e3f9ae8c42323cd44fcf66
Hiram Cruz <hiram0101@gmail.com>
2018-04-24 13:47:31 -0400

    formatting

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict"
 
 module.exports = {
   development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://localhost/noteful-app',
+    client: `pg`,
+    connection: process.env.DATABASE_URL || `postgres://localhost/noteful-app`,
     debug: true, // http://knexjs.org/#Installation-debug
-    pool: { min: 1, max: 2 }
+    pool: { min: 1, max: 2 },
   },
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL
-  }
-};
+    client: `pg`,
+    connection: process.env.DATABASE_URL,
+  },
+}
--- server.js
+++ server.js
@@ -1,49 +1,53 @@
-'use strict';
+"use strict"
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require(`express`)
+const morgan = require(`morgan`)
 
-const { PORT } = require('./config');
+const { PORT } = require(`./config`)
 
-const notesRouter = require('./routes/notes.router');
+const notesRouter = require(`./routes/notes.router`)
 
 // Create an Express application
-const app = express();
+const app = express()
 
 // Log all requests. Skip logging during
-app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
-  skip: () => process.env.NODE_ENV === 'test'
-}));
+app.use(
+  morgan(process.env.NODE_ENV === `development` ? `dev` : `common`, {
+    skip: () => process.env.NODE_ENV === `test`,
+  })
+)
 
 // Create a static webserver
-app.use(express.static('public'));
+app.use(express.static(`public`))
 
 // Parse request body
-app.use(express.json());
+app.use(express.json())
 
 // Mount router on "/api"
-app.use('/api', notesRouter);
+app.use(`/api`, notesRouter)
 
 // Catch-all 404
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use(function(req, res, next) {
+  const err = new Error(`Not Found`)
+  err.status = 404
+  next(err)
+})
 
 // Catch-all Error handler
 // Add NODE_ENV check to prevent stacktrace leak
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
   res.json({
     message: err.message,
-    error: app.get('env') === 'development' ? err : {}
-  });
-});
+    error: app.get(`env`) === `development` ? err : {},
+  })
+})
 
 // Listen for incoming connections
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
+app
+  .listen(PORT, function() {
+    console.info(`Server listening on ${this.address().port}`)
+  })
+  .on(`error`, err => {
+    console.error(err)
+  })
