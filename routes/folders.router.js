8de807f7166d6f192ac33890e2bf1f72f8d74a2d
Hiram Cruz <hiram0101@gmail.com>
2018-04-25 14:12:05 -0400

    fixed error in folders router

--- routes/folders.router.js
+++ routes/folders.router.js
@@ -1,7 +1,7 @@
 const express = require(`express`)
 const knex = require(`../knex`)
 
-const router = express.router()
+const router = express.Router()
 
 router.get(`/`, (req, res, next) => {
   // get all folders
