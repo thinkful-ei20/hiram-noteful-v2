365e93355a599a99ac0bb51824b7526c26933c63
Hiram Cruz <hiram0101@gmail.com>
2018-04-27 16:12:44 -0400

    updated database test seed

--- db/seedData.js
+++ db/seedData.js
@@ -2,7 +2,7 @@ const knex = require(`../knex`)
 const util = require(`util`)
 const exec = util.promisify(require(`child_process`).exec)
 
-module.exports = function(file, user = `shark`) {
+module.exports = function(file, user = `postgres`) {
   return exec(
     `psql -U ${user} -f ${file} ${knex.client.connectionSettings.database}`
   )
